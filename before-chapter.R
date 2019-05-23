4d6d98dcda83f054ee683fb525052fb5a57fcfd8
graphdr <graphdoctor@gmail.com>
2019-05-23 10:56:26 -0400

    renumber chapters

---
+++ before-chapter.R
@@ -0,0 +1,11 @@
+library("knitr")
+opts_chunk$set(echo = FALSE,
+        fig.width = 6,
+        out.width = "70%",
+        fig.align = "center",
+        fig.asp = 0.618,
+        comment = "#>",
+        warning = FALSE,
+        message = FALSE)
+
+suppressPackageStartupMessages(library("tidyverse"))
